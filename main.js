5bd104b9b146fc363cb8d58e9f4e80e20c0b82d1
meloni-creator <patricia.meloni.santos@escola.pr.gov.br>
2026-08-14 15:03:54 +0000

    Atualizar main.js

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ const gameOver = document.querySelector('.gameOver');
 
 const audioStart = new Audio('./soung/audio_theme.mp3');
 const audioGameOver = new Audio('./sound/audio_gameover.mp3');
+
+//variáveis de controle de jogo
+let gameInterval = null;
+
+//função para iniciar o jogo
+const startGame = () => {
+
+
+pipe.classList.add('pipe-animation');
+startButton.style.display = 'nome';
+gameOver.style.display = 'nome';
+audioStart.currentTime = 0;
+audioStart.play();
+     if(gameInterval){
+    gameInterval = setInterval(gameLoop, 10);
+}
+};
