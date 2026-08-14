5f462b7fa6a8251d2f9cbab2c236017866a7c685
meloni-creator <patricia.meloni.santos@escola.pr.gov.br>
2026-08-14 13:46:45 +0000

    Adicionar lógica inicial do jogo

---
+++ main.js
@@ -0,0 +1,11 @@
+//seleção de elementos
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const startButton = document.querySelector('.startButton');
+const restarButton = document.querySelector('.restarButton');
+const gameOver = document.querySelector('.gameOver');
+
+//definição de áudios
+
+const audioStart = new Audio('./soung/audio_theme.mp3');
+const audioGameOver = new Audio('./sound/audio_gameover.mp3');
